57bc1911f41cddf826f65307a2b9c34d4867f878
David Goeke <david.goeke@broadcom.com>
2016-05-24 13:56:27 -0700

    Add git prompt

--- config.fish
+++ config.fish
@@ -1,2 +1,18 @@
-set PATH ~/bin $PATH
+set -x GOPATH ~/go
+set PATH ~/bin $GOPATH/bin /usr/local/go/bin $PATH
 
+function fish_prompt --description 'Write out the prompt'
+  set -l last_status $status
+
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
+
+  __informative_git_prompt
+
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+  end
+
+  echo -n ' > '
+end
