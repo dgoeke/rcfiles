2aeefb89b65e8f6b91a5f7e249101a825c079744
David Goeke <dg@github.dgoeke.io>
2021-03-02 15:18:33 -0800

    Remove fancy tmux exit fn

--- .config/fish/functions/tmux_autostart.fish
+++ .config/fish/functions/tmux_autostart.fish
@@ -1,12 +1,4 @@
 function tmux_autostart
-  function exit
-    if [ "$TERM" = "screen-256color" ]
-      tmux detach -P
-    else
-      builtin exit
-    end
-  end
-
   if not set -q TMUX
     set -g TMUX tmux new-session -d -s base
     eval $TMUX
