d7e5ef85f5313c10f471779e760972a1563ce46c
David Goeke <dg@github.dgoeke.io>
2021-03-11 16:15:07 -0800

    Don't write machine.nix, just echo

--- gen_nix_machine.sh
+++ gen_nix_machine.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-echo "{ hostname = \"$(hostname)\"; operatingSystem = \"$(uname -v | awk '{ print $1 }' | sed 's/#.*-//')\"; }"> ~/.config/nixpkgs/machine.nix
+echo "{ hostname = \"$(hostname)\"; operatingSystem = \"$(uname -v | awk '{ print $1 }' | sed 's/#.*-//')\"; }"
