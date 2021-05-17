eabcf2bd4ff5f27e5571639dfbb9c8f45a0a3cdd
David Goeke <dg@github.dgoeke.io>
2021-05-16 17:12:11 -0700

    Add keybase

--- configuration.nix
+++ configuration.nix
@@ -59,6 +59,11 @@ in
   services = {
     openssh.enable = true;
     pcscd.enable = true;
+    kbfs = {
+      enable = true;
+      mountPoint = "%t/kbfs";
+      extraFlags = [ "-label %u" ];
+    };
 
     xserver = {
       enable = true;
@@ -72,6 +77,26 @@ in
       deviceSection = ''
         Option "TearFree" "true"
       '';
+    };
+  };
+
+  systemd.user.services = {
+    keybase.serviceConfig.Slice = "keybase.slice";
+
+    kbfs = {
+      environment = { KEYBASE_RUN_MODE = "prod"; };
+      serviceConfig.Slice = "keybase.slice";
+    };
+
+    keybase-gui = {
+      description = "Keybase GUI";
+      requires = [ "keybase.service" "kbfs.service" ];
+      after    = [ "keybase.service" "kbfs.service" ];
+      serviceConfig = {
+        ExecStart  = "${pkgs.keybase-gui}/share/keybase/Keybase";
+        PrivateTmp = true;
+        Slice      = "keybase.slice";
+      };
     };
   };
 
